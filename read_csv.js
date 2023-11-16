27981c2a91aa5fac5bf01b1da0293ae3149563cd
snickeristhebest <nicoh7242@gmail.com>
2023-11-16 16:14:21 -0600

    Can now view data reasonably well

--- read_csv.js
+++ read_csv.js
@@ -1,3 +1,40 @@
+// Define successFunction globally
+function successFunction(data) {
+    var allRows = data.split(/\r?\n|\r/);
+    var table = '<table>';
+    for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+        if (singleRow === 0) {
+            table += '<thead>';
+            table += '<tr>';
+        } else {
+            table += '<tr>';
+        }
+        var rowCells = allRows[singleRow].split(',');
+        for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+            if (singleRow === 0) {
+                table += '<th>';
+                table += rowCells[rowCell];
+                table += '</th>';
+            } else {
+                table += '<td>';
+                table += rowCells[rowCell];
+                table += '</td>';
+            }
+        }
+        if (singleRow === 0) {
+            table += '</tr>';
+            table += '</thead>';
+            table += '<tbody>';
+        } else {
+            table += '</tr>';
+        }
+    }
+    table += '</tbody>';
+    table += '</table>';
+    // $('body').append(table);
+    $('#table-container').html(table);
+}
+
 // Your existing JavaScript code goes here
 $(document).ready(function () {
     'use strict';
@@ -7,38 +44,17 @@ $(document).ready(function () {
         dataType: 'text',
     }).done(successFunction);
 
-    function successFunction(data) {
-        var allRows = data.split(/\r?\n|\r/);
-        var table = '<table>';
-        for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
-            if (singleRow === 0) {
-                table += '<thead>';
-                table += '<tr>';
-            } else {
-                table += '<tr>';
-            }
-            var rowCells = allRows[singleRow].split(',');
-            for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
-                if (singleRow === 0) {
-                    table += '<th>';
-                    table += rowCells[rowCell];
-                    table += '</th>';
-                } else {
-                    table += '<td>';
-                    table += rowCells[rowCell];
-                    table += '</td>';
-                }
-            }
-            if (singleRow === 0) {
-                table += '</tr>';
-                table += '</thead>';
-                table += '<tbody>';
-            } else {
-                table += '</tr>';
-            }
-        }
-        table += '</tbody>';
-        table += '</table>';
-        $('body').append(table);
+    // Define the handleButtonClick function
+    function handleButtonClick(csvFileName) {
+        alert('Button Clicked: ' + csvFileName);
+
+        // Update the CSV file URL based on the button clicked
+        var csvUrl = csvFileName;
+
+        // Fetch the data from the updated CSV file URL
+        $.ajax({
+            url: csvUrl,
+            dataType: 'text',
+        }).done(successFunction);
     }
 });
